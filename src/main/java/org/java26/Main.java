447e0a12b139e13a43826b1086571274b8c0afd2
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 09:22:23 +0200

    added date

--- src/main/java/org/java26/Main.java
+++ src/main/java/org/java26/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej världen");
         System.out.println("Mitt namn är Carmelo");
-        System.out.println("Dagens datum är 26 augusti 2026");
+        System.out.println("Dagens datum är 26 augusti 2026 ");
     }
 }
