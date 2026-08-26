5c59b24ddee7fe6bd5e7e943336bd8335519c1f2
Carmelo85S <148332645+Carmelo85S@users.noreply.github.com>
2026-08-26 09:20:30 +0200

    added name

--- src/main/java/org/java26/Main.java
+++ src/main/java/org/java26/Main.java
@@ -5,6 +5,7 @@ package org.java26;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej världen");
+        System.out.println("Mitt namn är Carmelo");
 
     }
 }
